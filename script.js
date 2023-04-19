487e2cdaa38d2774b6ac913095b612c8894e1b79
Larissa Martins <larissa.martins@inboundcycle.com>
2023-04-18 21:07:41 -0300

    Feat: add funcionality to the bolling button

---
+++ script.js
@@ -0,0 +1,65 @@
+'use strict';
+
+// Selecting elements
+const scoreElementO = document.getElementById('score--0');
+const scoreElement1 = document.getElementById('score--1');
+
+const currentElement0 = document.getElementById('current--0');
+const currentElement1 = document.getElementById('current--1');
+
+const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const playerElement0 = document.querySelector('.player--0');
+const playerElement1 = document.querySelector('.player--1');
+
+// Starting conditions
+let scores, currentScore, activePlayer, playing;
+
+const initGame = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    scoreElementO.textContent = 0;
+    scoreElement1.textContent = 0;
+    currentElement0.textContent = 0;
+    currentElement1.textContent = 0;
+    diceElement.classList.add('hidden');
+    playerElement0.classList.add('player--active');
+    playerElement1.classList.remove('player--active');
+    playing = true;
+};
+
+initGame();
+
+const switchPlayer = function () {
+    // set current score to 0
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+
+    // switch player        
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    playerElement0.classList.toggle('player--active');
+    playerElement1.classList.toggle('player--active');
+};
+
+// Rolling dice button functionality
+btnRoll.addEventListener('click', function () {
+    if (playing) {
+        const diceValue = Math.trunc(Math.random() * 6 + 1);
+
+        diceElement.classList.remove('hidden');
+        diceElement.src = `assets/dice-${diceValue}.png`;
+
+        if (diceValue !== 1) {
+            // add dice to current score
+            currentScore += diceValue;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        } else {
+            switchPlayer();
+        }
+    }
+});
